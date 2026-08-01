d0c06fa7fd83464bd5dcddfb3c41db33c9f4d5d1
Marcus <juandbeta@gmail.com>
2026-08-01 04:57:48 +0200

    host changed

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Big store API',
         description: 'API for products and customers',
     },
-    host: 'localhost:3000',
-    schemes: ['http']
+    host: 'cse341-final-project-x9ww.onrender.com',
+    schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
