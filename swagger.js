8dfacb2d4ab6920635bf9a46c3be9eb788ada9f8
Marcus <juandbeta@gmail.com>
2026-08-13 05:53:39 +0200

    enviroment updated

--- swagger.js
+++ swagger.js
@@ -5,8 +5,12 @@ const doc = {
         title: 'Big store API',
         description: 'API for products and customers',
     },
-    host: 'cse341-final-project-x9ww.onrender.com',
-    schemes: ['https'],
+    servers: [
+        {
+            url: 'https://cse341-final-project-x9ww.onrender.com',
+            description: 'Production Server'
+        }
+    ],
     components: {
         securitySchemes: {
             bearerAuth: {
